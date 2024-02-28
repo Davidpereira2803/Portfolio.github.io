fb63e984acf73cbad9cde1839e609f1b4921e24a
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-02-28 22:12:34 +0100

    Updated content plus icons

--- contentmanager.js
+++ contentmanager.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('projects-content/privatecontent.json')
-      .then(response => response.json())
-      .then(data => {
-        updateProjects(data.projects);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  });
+  fetch('projects-content/privatecontent.json')
+    .then(response => response.json())
+    .then(data => {
+      updateProjects(data.projects);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('projects-content/unicontent.json')
-      .then(response => response.json())
-      .then(data => {
-        updateUniProjects(data.projects);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  });
-  
+  fetch('projects-content/unicontent.json')
+    .then(response => response.json())
+    .then(data => {
+      updateUniProjects(data.projects);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
+
 function updateProjects(projects) {
-    const projectsContainer = document.getElementById('projects-container');
-  
-    if(projects.length < 2 ){ // error I am looking projects length but should be checking the project length instead -> how many elements in one project
+  const projectsContainer = document.getElementById('projects-container');
+
+  if (projects.length < 2) { // error I am looking projects length but should be checking the project length instead -> how many elements in one project
     projects.forEach(project => {
 
 
-      if(project.length == 5){
-      const projectElement = document.createElement('div');
-      projectElement.innerHTML = `
+      if (project.length == 5) {
+        const projectElement = document.createElement('div');
+        projectElement.innerHTML = `
         <h3>${project.name}</h3>
         <p>${project.description}</p>
         <p>Technologies: ${project.technologies.join(', ')}</p>
@@ -36,34 +36,36 @@ function updateProjects(projects) {
       `;
       }
       projectsContainer.appendChild(projectElement);
-    });}else{
-      projects.forEach(project => {
-        const projectElement = document.createElement('div');
-        projectElement.innerHTML = `
+    });
+  } else {
+    projects.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.innerHTML = `
           <h3>${project.name}</h3>
           <p>${project.description}</p>
           <p>Technologies: ${project.technologies.join(', ')}</p>
-          <a id="links" href="${project.url}" target="_blank">Visit Project</a>
+          <a href="${project.url}" target="_blank" rel="noopener noreferrer">
+          <i class="fa fa-github" style="font-size:48px;color:black"></i>
+          </a>
           <section id="content">
           <img id="mid-logo" src="${project.image}" alt="${project.name} Image">
           <img id="mid-logo" src="${project.image2}" alt="${project.name} Image">
           </section>
         `;
-    
-        projectsContainer.appendChild(projectElement);
-      });
-    }
-  }
 
-function updateUniProjects(projects) {
-    const projectsContainer = document.getElementById('uni-projects-container');
-  
-    projects.forEach(project => {
-      const projectElement = document.createElement('div');
-      projectElement.innerHTML = `<p>${project.content}</p>`;
-  
       projectsContainer.appendChild(projectElement);
     });
   }
+}
 
-  
+function updateUniProjects(projects) {
+  const projectsContainer = document.getElementById('uni-projects-container');
+
+  projects.forEach(project => {
+    const projectElement = document.createElement('div');
+    projectElement.innerHTML = `<p>${project.content}</p>`;
+
+    projectsContainer.appendChild(projectElement);
+  });
+}
+
